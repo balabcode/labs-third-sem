b7960b3b647276cdfc4768a5d3aeaa0fbe11bd91
lab-codes-ez <144760458+lab-codes-ez@users.noreply.github.com>
2023-10-05 19:32:25 +0530

    Add files via upload

--- 220962448_Balaji/Lab7/Building.java
+++ 220962448_Balaji/Lab7/Building.java
@@ -1,122 +1,26 @@
 package com.course.structure;
-import java.util.*;
+import com.course.structure.*;
+import java.util.Scanner;
 
-class building{
-    private double squareFootage;
-    private int stories;
+public class Building {
+    Scanner sc = new Scanner(System.in);
 
-    void setSquareFootage(double squareFootage){
-        this.squareFootage=squareFootage;
-    }
-    void setStories(int stories){
-        this.stories=stories;
-    }
-    double getSquareFootage(){
-        return this.squareFootage;
-    }
-    int getStories(){
-        return this.stories;
-    }
-}
+    double sqFootage;
+    int stories;
 
-class house extends building{
-    private int bedrooms;
-    private int bathrooms;
-
-    void setBedrooms(int bedrooms){
-        this.bedrooms=bedrooms;
+    public Building() {
+        sqFootage = -1;
+        stories = -1;
     }
 
-    void setBathrooms(int bathrooms){
-        this.bathrooms=bathrooms;
+    public void assign() {
+        System.out.print("Enter the square footage ");
+        sqFootage = sc.nextDouble();
+        System.out.print("Enter number of stories ");
+        stories = sc.nextInt();
     }
 
-    int getBedrooms(){
-        return this.bedrooms;
-    }
-
-    int getBathrooms(){
-        return this.bathrooms;
-    }
-}
-
-class school extends house{
-    private int classrooms;
-    private String gradeLevel;
-
-    void setClasrooms(int classrooms){
-        this.classrooms=classrooms;
-    }
-
-    void setGradeLevel(String gradeLevel){
-        this.gradeLevel=gradeLevel;
-    }
-
-    int getClassrooms(){
-        return classrooms;
-    }
-
-    String getGradeLevel(){
-        return gradeLevel;
-    }
-
-    void display(int choice){
-        System.out.println("\nDisplaying Entered Data: \n");
-        System.out.println("Square Footage: " + getSquareFootage());
-        System.out.println("Stories: " + getStories());
-
-        if(choice==1){
-            System.out.println("Bedrooms: " + getBedrooms());
-            System.out.println("Bathrooms: " + getBathrooms());
-        }
-        else if(choice==2){
-            System.out.println("Classrooms: " + getClassrooms());
-            System.out.println("Grade Level: " + getGradeLevel());
-        }
-    }
-}
-
-
-class Building{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        school obj = new school();
-
-        System.out.println("Enter the following Details: \n");
-        System.out.print("Square Footage: ");
-        double squareFootage = sc.nextDouble();
-        obj.setSquareFootage(squareFootage);
-
-        System.out.print("Stories: ");
-        int stories = sc.nextInt();
-        obj.setStories(stories);
-
-        System.out.println("\nSelect the correct option:");
-        System.out.println("1. House");
-        System.out.println("2. School");
-        System.out.print("Option: ");
-        int choice = sc.nextInt();
-        if(choice==1){
-            System.out.print("\nBedrooms: ");
-            int bedrooms = sc.nextInt();
-            obj.setBedrooms(bedrooms);
-
-            System.out.print("Bathrooms: ");
-            int bathroms = sc.nextInt();
-            obj.setBathrooms(bathroms);
-        }
-        else if (choice==2){
-            System.out.print("\nClassrooms: ");
-            int classrooms = sc.nextInt();
-            obj.setClasrooms(classrooms);
-
-            System.out.print("Grade Level: ");
-            sc.nextLine();
-            String gradeLevel = sc.nextLine();
-            obj.setGradeLevel(gradeLevel);
-        }
-        obj.display(choice);
-        sc.close();
+    public void display() {
+        System.out.println("Square Footage: "+sqFootage+"\nStories: "+stories);
     }
 }
--- 220962448_Balaji/Lab8/StudentDemo.java
+++ 220962448_Balaji/Lab8/StudentDemo.java
@@ -1,48 +1,51 @@
 import java.util.Scanner;
-class SeatsFilledException extends Exception{
-    public String toString(){
+
+class SeatsFilledException extends Exception {
+    public String toString() {
         return "No seats left! Max is 25 students.";
     }
 }
-class Student{
+
+class Student {
     String name;
     String regno = "";
     String doj;
     static int count = 0;
     Scanner sc = new Scanner(System.in);
-    Student() throws SeatsFilledException{
+
+    Student() throws SeatsFilledException {
         System.out.print("Enter name: ");
         name = sc.nextLine();
         System.out.print("Enter date of joining: ");
         doj = sc.nextLine();
         count++;
-        regno += doj.charAt(doj.length() -2);
-        regno += doj.charAt(doj.length() -1);
-        if (count > 25){
+        regno += doj.charAt(doj.length() - 2);
+        regno += doj.charAt(doj.length() - 1);
+        if (count > 25) {
             throw new SeatsFilledException();
         }
-        if (count < 10){
+        if (count < 10) {
             regno += "0" + count;
-        }
-        else{
+        } else {
             regno += count;
         }
     }
-    public void display(){
-        System.out.println("Name: "+ this.name);
-        System.out.println("Year: "+ doj);
-        System.out.print("RegNo: "+this.regno+"\n");
+
+    public void display() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Year: " + doj);
+        System.out.print("RegNo: " + this.regno + "\n");
     }
 }
-class StudentDemo{
-    public static void main(String[] args){
+
+class StudentDemo {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for(int i = 0; i < 30; i++){
-            try{
+        for (int i = 0; i < 30; i++) {
+            try {
                 Student student = new Student();
                 student.display();
-            }
-            catch(SeatsFilledException e){
+            } catch (SeatsFilledException e) {
                 System.out.println(e);
             }
         }
